66914d82736fc8637140077aeae72a1a63330e76
LadyViktoria <leonieneon@gmail.com>
2017-03-14 23:38:50 +0100

    add sample data

---
+++ src/com/lady/viktoria/Main.java
@@ -0,0 +1,15 @@
+package com.lady.viktoria;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // example data
+        double[] sensorRawData = { 80, 85, 95 };
+        double[] bgMeterData = { 0, 0.1, 0.2  };
+        double[] weight = { 1, 1, 1};
+
+        double[] y = sensorRawData;
+        double[] x = bgMeterData;
+
+    }
+}
