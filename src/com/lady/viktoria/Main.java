443f3eeed3347eea6af6a1488664594ef01e434e
LadyViktoria <leonieneon@gmail.com>
2017-03-15 00:41:59 +0100

    get min max value

--- src/com/lady/viktoria/Main.java
+++ src/com/lady/viktoria/Main.java
@@ -5,27 +5,45 @@ public class Main {
     public static void main(String[] args) {
         // example data
         double[] sensorRawData = { 80, 85, 95 };
-        double[] bgMeterData = { 160, 150 };
+        double[] bgMeterData = { 180, 160 };
         double[] weight = { 1, 1, 1};
-
-        double[] y = sensorRawData;
-        double[] x = bgMeterData;
 
         // one pint calibration
         double b = 0;
         double m = 0;
 
-        for (int i=0; i<y.length; i++) {
-            m = (y[i] - b) / x[0];
-            System.out.println("one point calibration for raw value " + y[i] + " bgMeterData: " + x[0] + " m is: " + m);
+        for (int i = 0; i< sensorRawData.length; i++) {
+            m = (sensorRawData[i] - b) / bgMeterData[0];
+            System.out.println("one point calibration for " + sensorRawData[i] + " bgMeterData: " + bgMeterData[0] + " m is: " + m);
         }
 
-        System.out.println("____________________");
+        System.out.println("---------------------");
         // two point calibration
+        // m = ( y2 - y1 ) * ( x2 - x1 )
+        // b = y2 - mx2
         for (int i=0; i < 1; i++) {
-            m=(y[1] - y[0]) * (x[1] - x[0]);
-            b=y[1] - m * x[1];
-            System.out.println("two point calibration for " + y[0] + " m is " + m + " b is " + b);
+            m=(getMax(sensorRawData[0], sensorRawData[1]) - getMin(sensorRawData[0], sensorRawData[1]))
+                    * (getMax(bgMeterData[0], bgMeterData[1]) - getMin(bgMeterData[0], bgMeterData[1]));
+            b= sensorRawData[1] - m * bgMeterData[1];
+            System.out.println("two point calibration for " + sensorRawData[0] + " m is " + m + " b is " + b);
         }
     }
+
+    public static double getMax(double a, double b){
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static double getMin(double a, double b){
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+
 }
